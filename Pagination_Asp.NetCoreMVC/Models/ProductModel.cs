a7938d35e5a395e2bedcc416c8b3cd175dfed99e
Keval Elsner <kevalmaisuriyaelsner@gmail.com>
2022-06-08 13:09:17 +0530

    Create Product Model class

---
+++ Pagination_Asp.NetCoreMVC/Models/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Pagination_Asp.NetCoreMVC.Models
+{
+    public class ProductModel
+    {
+        public List<Product> Products { get; set; }
+
+        public int CurrentIndex { get; set; }
+
+        public int PageCount { get; set; }
+    }
+
+    public class Product
+    {
+        public string PName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Manufacturer { get; set; }
+    }
+}
+ 
